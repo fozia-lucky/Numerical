1c92187b1b17cd3571810c367ee0e97d4a15c55d
fozia-lucky <81360379+fozia-lucky@users.noreply.github.com>
2021-03-25 16:59:56 +0600

    Add files via upload

---
+++ Bankers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    double a;
+    scanf("%d",&a);
+    if ((t < 0.5) || (t == 0.5 && s % 2 == 0))
+  {
+    return s;
+  }
+
+}
